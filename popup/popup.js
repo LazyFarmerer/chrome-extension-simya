cb5c2bf8c55107ba78dd20f24c25852f6bd9b81d
Lazyfarmerer <131164959+Lazyfarmerer@users.noreply.github.com>
2025-01-17 01:56:14 +0900

    popup창 꾸미기 & 기능구현

--- popup/popup.js
+++ popup/popup.js
@@ -1,5 +1,39 @@
 
 
-document.querySelector("button").addEventListener("click", (event) => {
-    alert("클릭함!");
-});
+const current_manifest_version_element = document.getElementById("current_manifest_version");
+const check_result = document.getElementById("check-result");
+const version_check_button = document.getElementById("version_check");
+
+setTimeout(async () => {
+    const current_manifest = await requests("./../manifest.json");
+    current_manifest_version_element.textContent = current_manifest.version;
+}, 500);
+
+document.getElementById("version_check").onclick = async () => {
+    await version_check();
+  };
+
+
+async function version_check() {
+    const github_manifest_version_element = document.getElementById("github_manifest_version");
+
+    github_manifest_version_element.classList.toggle("display-none");
+    check_result.classList.toggle("display-none");
+    version_check_button.classList.toggle("display-none");
+
+    const github_manifest = await requests("https://cdn.jsdelivr.net/gh/LazyFarmerer/chrome-extension-simya@main/manifest.json");
+
+    github_manifest_version_element.textContent = github_manifest.version;
+    const result = (current_manifest_version_element.textContent === github_manifest.version)
+                    ? "같은 버전이네요 :D"
+                    : "버전이 다르네요 :<";
+
+    check_result.textContent = result;
+}
+
+
+
+async function requests(url) {
+    const req = await fetch(url);
+    return req.json();
+}
