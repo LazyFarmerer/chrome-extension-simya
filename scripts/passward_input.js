2893f1b3455b8d2426b9f721651d03da689655a9
Lazyfarmerer <131164959+Lazyfarmerer@users.noreply.github.com>
2025-01-18 01:27:16 +0900

    패스워드 찾기 살짝 수정

--- scripts/passward_input.js
+++ scripts/passward_input.js
@@ -2,21 +2,28 @@
 
 const passward = "smpeople";
 
-
-// 1초 마다 실행, 찾으면 입력 후 종료
-let intervalId = setInterval(() => {
-
-    let inputElement = getInputElement();
-    if (inputElement) {
-        inputElement.value = passward;
+// 한번 찾기 시도 후 없다면
+// 5초간 매 초 찾기 시도
+let inputElement = getInputElement();
+if (inputElement) {
+    inputElement.value = passward;
+}
+else {
+    // 1초 마다 실행, 찾으면 입력 후 종료
+    let intervalId = setInterval(() => {
+    
+        let inputElement = getInputElement();
+        if (inputElement) {
+            inputElement.value = passward;
+            clearInterval(intervalId);
+        }
+    }, 1000);
+    
+    // 5초 뒤 종료
+    setTimeout(() => {
         clearInterval(intervalId);
-    }
-}, 1000);
-
-// 5초 뒤 종료
-setTimeout(() => {
-    clearInterval(intervalId);
-}, 5000);
+    }, 5000);
+}
 
 
 // 인풋 요소 찾기
