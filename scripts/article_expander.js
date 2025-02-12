f5b5b86a760fd0ac20e0c277338a3ab235d221f2
Lazyfarmerer <131164959+Lazyfarmerer@users.noreply.github.com>
2025-02-12 22:36:43 +0900

    1.4.0 제출용

--- scripts/article_expander.js
+++ scripts/article_expander.js
@@ -20,29 +20,31 @@ function initializeArticleExpander() {
   // 버튼 만들기
   const extendButton = createExtendButton();
   mainContent.querySelector(".article-link").prepend(extendButton);
-  // mainContent.querySelector(".article-menu").prepend(extendButton);
 
   // 조작할 요소들 가져오기
-  // 그냥 html 태그 -> width-1300 제거용
+  // 그냥 html 태그 -> width-... 제거용
   const root = document.querySelector("html");
+  const widthClassName = Array.from(root.classList).find(cls => cls.startsWith("width-"));
   const leftSideBar = document.querySelector("div.sidebar.left-ad-area");
   const rightSideBar = document.querySelector("aside.sidebar.right-sidebar");
 
   extendButton.addEventListener("click", () => {
     const isExtended = extendButton.getAttribute("data-extended") === "true";
     extendButton.setAttribute("data-extended", !isExtended);
-    extendButton.textContent = isExtended ? "펼치기" : "줄이기";
+    extendButton.innerHTML = isExtended
+    ? `<span class="ion-arrow-expand icon"></span> 펼치기`
+    : `<span class="ion-arrow-shrink icon"></span> 줄이기`;
 
 
     // 요소들 조작하기
     // 최대너비 제거하고 없앨거 없애기
     if (isExtended) {
-      root.classList.add("width-1300");
+      if (widthClassName) root.classList.add(widthClassName);
       mainContent.classList.remove("simya-extend-main-content");
       leftSideBar.classList.remove("simya-display-none");
       rightSideBar.classList.remove("simya-display-none");
     } else {
-      root.classList.remove("width-1300");
+      if (widthClassName) root.classList.remove(widthClassName);
       mainContent.classList.add("simya-extend-main-content");
       leftSideBar.classList.add("simya-display-none");
       rightSideBar.classList.add("simya-display-none");
@@ -53,50 +55,9 @@ function initializeArticleExpander() {
 
 function createExtendButton() {
     const button = document.createElement("button");
-    button.textContent = "펼치기";
+    button.innerHTML = `<span class="ion-arrow-expand icon"></span> 펼치기`;
+    button.title = "심야 확장 펼치기";
     button.setAttribute("data-extended", "false");
     button.classList.add("simya-extend-button", "btn", "btn-sm", "btn-arca", "btn-arca-article-write");
     return button;
   }
-
-
-
-
-
-
-
-// if (articleWrapper) {
-//   const articleLink = articleWrapper.querySelector(".article-link");
-//   const extendButtonElement = extendButton();
-//   const shrinkButtonElement = shrinkButton();
-  
-//   articleLink.prepend(extendButtonElement);
-//   articleLink.prepend(shrinkButtonElement);
-
-//   extendButtonElement.addEventListener("click", () => {
-//     console.log("펼치기");
-//     extendButtonElement.classList.add("simya-display-none");
-//     shrinkButtonElement.classList.remove("simya-display-none");
-//   });
-
-//   shrinkButtonElement.addEventListener("click", () => {
-//     console.log("줄이기");
-//     extendButtonElement.classList.remove("simya-display-none");
-//     shrinkButtonElement.classList.add("simya-display-none");
-//   });
-// }
-
-
-// function extendButton() {
-//     const button = document.createElement("button");
-//     button.textContent = "펼치기";
-//     button.classList.add("simya-extend-button", "btn", "btn-sm", "btn-arca", "btn-arca-article-write");
-//     return button;
-//   }
-
-// function shrinkButton() {
-//   const button = document.createElement("button");
-//   button.textContent = "줄이기";
-//   button.classList.add("simya-shrink-button", "simya-display-none", "btn", "btn-sm", "btn-arca", "btn-arca-article-write");
-//   return button;
-// }
